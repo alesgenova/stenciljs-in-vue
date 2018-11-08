561735acf3726251cd9d642c8b5ae9b3ef944795
Alessandro Genova <ales.genova@gmail.com>
2018-11-07 21:53:29 -0500

    Update dependencies and instructions

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import { defineCustomElements as defineMolecule } from '@openchemistry/molecule-vtkjs';
-import { defineCustomElements as defineSplitMe } from 'split-me';
+import { defineCustomElements as defineMolecule } from '@openchemistry/molecule-vtkjs/dist/loader';
+import { defineCustomElements as defineSplitMe } from 'split-me/dist/loader';
 
 defineMolecule(window);
 defineSplitMe(window);
